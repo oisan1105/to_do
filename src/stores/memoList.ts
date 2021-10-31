a8840b338464826534c9f18cb7706d7510e2e40a
BuildTools <unconfigured@null.spigotmc.org>
2021-10-31 22:08:23 +0900

    ローカルストレージで同期

--- src/stores/memoList.ts
+++ src/stores/memoList.ts
@@ -5,21 +5,36 @@ import  { MemoType } from '../type/Memo'
 
 
 // Stateの初期状態
-const initialState:MemoType[] = [];
+const storage = localStorage.getItem('memoList');
+const initialState:MemoType[] = storage === null ? []:JSON.parse(storage,reviver);
+function reviver(key:string, val:any){
+  if (typeof(val) == "string" &&
+      val.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/)){
+      return new Date(Date.parse(val));
+  }
+  return val;
+}
+
+function updateStorage(state:MemoType[]){
+localStorage.setItem('memoList', JSON.stringify(state))
+}
 
 const add: CaseReducer<MemoType[], PayloadAction<MemoType>> = (state, action) => {
   console.log(state,action)
-  return [...state,action.payload]
+  const memoList = [...state,action.payload]
+  updateStorage(memoList);
+  return memoList;
 }
 
 const remove: CaseReducer<MemoType[], PayloadAction<string>> = (state, action) => {
   console.log(state,action)
   const id = action.payload
-  const result = state.filter(function( item ){
+  const memoList = state.filter(function( item ){
     console.log(item, id)
     return item.id !== id
-      });
-  return result;
+  });
+  updateStorage(memoList);
+  return memoList;
 }
 
 const favorite: CaseReducer<MemoType[], PayloadAction<{id:string, favorite:boolean}>> = (state, action) => {
@@ -30,6 +45,7 @@ const favorite: CaseReducer<MemoType[], PayloadAction<{id:string, favorite:boole
     }
   
   state[targetMemoIndex].favorite = favorite;
+  updateStorage(state);
   return state;
   }
 
