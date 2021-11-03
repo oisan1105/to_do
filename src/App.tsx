3795b05d002b39c011d82dd0bfa21bd0ad90f62f
BuildTools <unconfigured@null.spigotmc.org>
2021-11-03 21:16:46 +0900

    Modalの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from "react-modal";
 import Sub from './component/Sub';
 import Main from './component/Main';
 import './App.css';
 import AddButton from './component/AddButton';
 import { useDispatch } from 'react-redux';
 import { addMemo, deleteMemo } from './stores/memoList';
-import { MemoType } from './type/Memo'
+import { MemoType } from './type/Memo';
+import AddMemoForm from './component/AddMemoForm';
 
 
 function App() {
+  const [modalIsOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch();
   const Add = () =>{
     const id:string = getId()
@@ -35,9 +38,11 @@ function App() {
       <div className='container main' ><Main /></div>
       <div className='container sub' ><Sub /></div>
       <div className='add-button'>
-        <AddButton Add={Add} />
-
+        <AddButton Add={() => setIsOpen(true)} />
       </div>
+      <Modal isOpen={modalIsOpen}>
+        <AddMemoForm Close={() => setIsOpen(false)} />
+      </Modal>
     </div>
   );
 }
